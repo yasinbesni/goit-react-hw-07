d5c4924b1efadf1f5e8b4dbb76dd7d0614ba663d
Yasin Besni <125811137+yasinbesni@users.noreply.github.com>
2025-08-21 18:53:38 +0300

    delete env because vercel undefinded

--- goit-react-hw-07/src/redux/contactsOps.js
+++ goit-react-hw-07/src/redux/contactsOps.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = "https://68a7381b639c6a54e9a16284.mockapi.io/";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
