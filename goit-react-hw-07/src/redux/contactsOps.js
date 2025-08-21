8cfb9a3f901c38b219ce499c19dd63a0fda69869
Yasin Besni <125811137+yasinbesni@users.noreply.github.com>
2025-08-21 18:40:52 +0300

    updated contactsOps.js

--- goit-react-hw-07/src/redux/contactsOps.js
+++ goit-react-hw-07/src/redux/contactsOps.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;;
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
