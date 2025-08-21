b4d4de37e98724c91f72532036c6c973f335462d
Yasin Besni <125811137+yasinbesni@users.noreply.github.com>
2025-08-21 17:59:28 +0300

    App path corrected

--- goit-react-hw-07/src/main.jsx
+++ goit-react-hw-07/src/main.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import App from "./components/app/App.jsx";
+import App from "./components/App/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
